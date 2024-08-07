131ea6634e57ea2330f1b8134a2e0df5da7a2924
Jiri Friedrich <jiri.friedrich@icloud.com>
2024-08-07 23:16:03 +0200

    Init library

---
+++ lib/components/Button.tsx
@@ -0,0 +1,5 @@
+function Button() {
+  return <button>Hello world!</button>;
+}
+
+export { Button };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: "lib/index.ts",
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+  plugins: [
+    react(),
+    dts({ include: ["lib"], tsconfigPath: "./tsconfig.build.json" }),
+  ],
+});
